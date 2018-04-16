a84c49f9ae5975e422aaf919069266314994b84a
ryan17 <ryangunawan33@gmail.com>
2018-04-16 10:45:24 +0700

    Changes : - Add Style1.css - Change bundle config

--- WebPallete/App_Start/BundleConfig.cs
+++ WebPallete/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace WebPallete
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Style1.css"));
         }
     }
 }
