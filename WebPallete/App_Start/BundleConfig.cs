4600db4ad1e07011bd06526a34dd293372c3b4fe
kevingautama <kevingautama97@gmail.com>
2018-04-16 11:51:58 +0700

    Add Javascript Function

--- WebPallete/App_Start/BundleConfig.cs
+++ WebPallete/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace WebPallete
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Style2.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
