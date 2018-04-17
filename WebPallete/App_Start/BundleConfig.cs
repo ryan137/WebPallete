884f6a095f5f74a9f1e3b0dc6261d0d09e0e9a4a
ryan17 <ryangunawan33@gmail.com>
2018-04-17 09:37:23 +0700

    Move to themes setting

--- WebPallete/App_Start/BundleConfig.cs
+++ WebPallete/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebPallete
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/content/bootstrap.css",
                       "~/Content/Site.css"));
         }
     }
